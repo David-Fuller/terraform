644c9d2f2d5a3341b92c4779993350a20090beb1
David-Fuller <dpfuller66@gmail.com>
2018-03-13 07:23:43 +0000

    added IP output

--- main.tf
+++ main.tf
@@ -1,31 +1,36 @@
 provider "aws" {
   region = "eu-west-2"
 }
+
 resource "aws_instance" "my-instance" {
-  ami           = "ami-403e2524"
-  instance_type = "t2.micro"
-  key_name = "LondonKeyPair"
-  vpc_security_group_ids = ["${aws_security_group.mysg.id}"]
+  ami                    = "ami-403e2524"
+  instance_type          = "t2.micro"
+  key_name               = "LondonKeyPair"
+  vpc_security_group_ids = ["${aws_security_group.my-sg.id}"]
+
   user_data = <<-EOF
- #!/bin/bash
- sudo yum install httpd -y
- sudo yum update -y
- sudo service httpd start
- sudo chkconfig httpd on
- cd /var/www/html
- echo "<h1>Hello World</h1>" > index.html
- EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
+
   tags {
     Name = "terraform-example"
   }
 }
-resource "aws_security_group" "mysg" {
-  name = "terraform-example-sg"
+
+resource "aws_security_group" "my-sg" {
+  name = "terraform-sg"
+
   ingress {
-    from_port = 8080
-    to_port   = 8080
-    protocol  = "tcp"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
+output "public_ip" {
+  value = "${aws_instance.my-instance.public_ip}"
+}
+          
